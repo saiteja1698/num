d18dc0ed96ff0397e442bf92bb6af62c9efbb41f
Saiteja <52398704+saiteja1698@users.noreply.github.com>
2019-07-03 09:51:04 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,9 @@
 s=input()
 sets = ["a","e","i","o","u"]
+set1 = ["!","@","#","$","%","^","&","*","(","+"]
 if s in sets:
   print("vowels")
+elif s in set1:
+    print("invalid")
 else:
    print("consonant") 
